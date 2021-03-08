6a982364aa569ecef94a46fa08a892492d0af764
颜洪毅 <yhyzgn@gmail.com>
2021-03-08 15:59:11 +0800

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import {App} from 'vue'
 import mitt from 'mitt'
-import {getOption, InstallerOption, setOption} from './option'
 
 const emitter = mitt()
 
 const EmitterBus = {
-  install (app: App<any>, opts?: InstallerOption): void {
-    if (opts) {
-      setOption(opts)
-    }
-    opts = getOption()
-    app.config.globalProperties[opts.propertyName] = emitter
+  install (app: App<any>): void {
+    app.config.globalProperties.$bus = emitter
   }
 }
 
